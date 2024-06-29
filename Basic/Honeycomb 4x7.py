a5d5867c9aab37a17cb78db36db642560208717e
adwaith <adwaithpda@gmail.com>
2024-06-29 13:17:43 +0530

    Initial commit

---
+++ Basic/Honeycomb 4x7.py
@@ -0,0 +1,33 @@
+def print_honeycomb(rows, columns):
+    if rows <= 0 or columns <= 0:
+        print("Row and colum are positive")
+        return
+
+    for row in range(rows):
+        if row % 2 == 1:
+            print("  ", end="")
+
+        #  top
+        for col in range(columns):
+            print("  _   ", end="")
+        print()
+
+        #  upper middle
+        if row % 2 == 1:
+            print("  ", end="")
+        for col in range(columns):
+            print(" / \\ ", end="")
+        print()
+
+        #  lower middle
+        if row % 2 == 1:
+            print("  ", end="")
+        for col in range(columns):
+            print(" \\_/ ", end="")
+        print()
+
+
+# Example
+rows = 4
+columns = 7
+print_honeycomb(rows, columns)
